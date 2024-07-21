1d8adf0429e7e29ff1664e66135e8aac7c73c1a8
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-07-22 00:59:43 +0300

    Added scripts for CS and SC packets;

---
+++ Scripts/script_rename_x-refs_CS_PACKETS_return_0.py
@@ -0,0 +1,38 @@
+import idaapi
+import idautils
+import idc
+
+def read_rename_list(file_path):
+    rename_dict = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 2:
+                old_name, new_name = parts
+                rename_dict[old_name] = new_name
+    return rename_dict
+
+def rename_xrefs(target_name, rename_dict):
+    target_addr = idc.get_name_ea_simple(target_name)
+    if target_addr == idc.BADADDR:
+        print(f"Target name '{target_name}' not found.")
+        return
+
+    for xref in idautils.XrefsTo(target_addr):
+        xref_addr = xref.frm
+        xref_name = idc.get_name(xref_addr)
+        if xref_name in rename_dict:
+            new_name = rename_dict[xref_name]
+            if idc.set_name(xref_addr, new_name, idc.SN_CHECK):
+                print(f"Renamed {xref_name} to {new_name}")
+            else:
+                print(f"Failed to rename {xref_name} to {new_name}")
+
+def main():
+    file_path = "CSOffsets.cs_IDA.txt"  # Укажите путь к вашему текстовому файлу
+    rename_dict = read_rename_list(file_path)
+    target_name = "CS_PACKETS_return_0"
+    rename_xrefs(target_name, rename_dict)
+
+if __name__ == "__main__":
+    main()
